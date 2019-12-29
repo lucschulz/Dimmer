96f0ff773e21f6808c944b6e674a4484396d38cc
Luc Schulz <luc@lucschulz.com>
2019-12-29 18:08:47 -0500

    ls

--- Dimmer/Properties/AssemblyInfo.cs
+++ Dimmer/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Dimmer")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright © 2019 - Luc Schulz")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
