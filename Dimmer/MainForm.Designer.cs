d02da2736e1c2c471379345155861bf4727ae4b7
lucschulz <luc@lucschulz.com>
2021-10-27 12:45:46 -0400

    Adding tbar for second monitor

--- Dimmer/MainForm.Designer.cs
+++ Dimmer/MainForm.Designer.cs
@@ -28,45 +28,87 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Dimmer));
-            this.tbar = new System.Windows.Forms.TrackBar();
-            ((System.ComponentModel.ISupportInitialize)(this.tbar)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // tbar
-            // 
-            this.tbar.LargeChange = 10;
-            this.tbar.Location = new System.Drawing.Point(12, 12);
-            this.tbar.Maximum = 255;
-            this.tbar.Name = "tbar";
-            this.tbar.Size = new System.Drawing.Size(154, 45);
-            this.tbar.SmallChange = 5;
-            this.tbar.TabIndex = 2;
-            this.tbar.Value = 255;
-            this.tbar.Scroll += new System.EventHandler(this.Tbar_Scroll);
-            // 
-            // Dimmer
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(178, 61);
-            this.Controls.Add(this.tbar);
-            this.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.MaximizeBox = false;
-            this.MinimizeBox = false;
-            this.Name = "Dimmer";
-            this.Text = "Dimmer";
-            ((System.ComponentModel.ISupportInitialize)(this.tbar)).EndInit();
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Dimmer));
+			this.tbarMonitorOne = new System.Windows.Forms.TrackBar();
+			this.tbarMonitorTwo = new System.Windows.Forms.TrackBar();
+			this.lblMonitorOne = new System.Windows.Forms.Label();
+			this.lblMonitorTwo = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.tbarMonitorOne)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.tbarMonitorTwo)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// tbarMonitorOne
+			// 
+			this.tbarMonitorOne.LargeChange = 10;
+			this.tbarMonitorOne.Location = new System.Drawing.Point(12, 56);
+			this.tbarMonitorOne.Maximum = 255;
+			this.tbarMonitorOne.Name = "tbarMonitorOne";
+			this.tbarMonitorOne.Size = new System.Drawing.Size(404, 56);
+			this.tbarMonitorOne.SmallChange = 5;
+			this.tbarMonitorOne.TabIndex = 2;
+			this.tbarMonitorOne.Value = 255;
+			this.tbarMonitorOne.Scroll += new System.EventHandler(this.Tbar_Scroll);
+			// 
+			// tbarMonitorTwo
+			// 
+			this.tbarMonitorTwo.LargeChange = 10;
+			this.tbarMonitorTwo.Location = new System.Drawing.Point(12, 165);
+			this.tbarMonitorTwo.Maximum = 255;
+			this.tbarMonitorTwo.Name = "tbarMonitorTwo";
+			this.tbarMonitorTwo.Size = new System.Drawing.Size(404, 56);
+			this.tbarMonitorTwo.SmallChange = 5;
+			this.tbarMonitorTwo.TabIndex = 3;
+			this.tbarMonitorTwo.Value = 255;
+			// 
+			// lblMonitorOne
+			// 
+			this.lblMonitorOne.AutoSize = true;
+			this.lblMonitorOne.ForeColor = System.Drawing.Color.Snow;
+			this.lblMonitorOne.Location = new System.Drawing.Point(12, 30);
+			this.lblMonitorOne.Name = "lblMonitorOne";
+			this.lblMonitorOne.Size = new System.Drawing.Size(108, 23);
+			this.lblMonitorOne.TabIndex = 4;
+			this.lblMonitorOne.Text = "Monitor One";
+			// 
+			// lblMonitorTwo
+			// 
+			this.lblMonitorTwo.AutoSize = true;
+			this.lblMonitorTwo.ForeColor = System.Drawing.Color.Snow;
+			this.lblMonitorTwo.Location = new System.Drawing.Point(12, 139);
+			this.lblMonitorTwo.Name = "lblMonitorTwo";
+			this.lblMonitorTwo.Size = new System.Drawing.Size(106, 23);
+			this.lblMonitorTwo.TabIndex = 5;
+			this.lblMonitorTwo.Text = "Monitor Two";
+			// 
+			// Dimmer
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.Black;
+			this.ClientSize = new System.Drawing.Size(428, 233);
+			this.Controls.Add(this.lblMonitorTwo);
+			this.Controls.Add(this.lblMonitorOne);
+			this.Controls.Add(this.tbarMonitorTwo);
+			this.Controls.Add(this.tbarMonitorOne);
+			this.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "Dimmer";
+			this.Text = "Dimmer";
+			((System.ComponentModel.ISupportInitialize)(this.tbarMonitorOne)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.tbarMonitorTwo)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.TrackBar tbar;
-    }
+        private System.Windows.Forms.TrackBar tbarMonitorOne;
+		private System.Windows.Forms.TrackBar tbarMonitorTwo;
+		private System.Windows.Forms.Label lblMonitorOne;
+		private System.Windows.Forms.Label lblMonitorTwo;
+	}
 }
 
--- Dimmer/MainForm.cs
+++ Dimmer/MainForm.cs
@@ -20,8 +20,8 @@ namespace Dimmer
 
         private void Tbar_Scroll(object sender, System.EventArgs e)
         {
-            BrightnessController.MonitorArray[0].SetBrightness((int)tbar.Value);
-            BrightnessController.MonitorArray[1].SetBrightness((int)tbar.Value);
+            BrightnessController.MonitorArray[0].SetBrightness((int)tbarMonitorOne.Value);
+            BrightnessController.MonitorArray[1].SetBrightness((int)tbarMonitorOne.Value);
         }
     }
 }
