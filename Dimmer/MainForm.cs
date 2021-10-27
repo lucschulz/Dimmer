f052c53b48fd39b41c602c18437a93095fc5f252
Luc Schulz <luc@lucschulz.com>
2021-10-27 15:26:36 -0400

    Second monitor (#1)
    * added reset button
    
    * Added reset button

--- Dimmer/MainForm.cs
+++ Dimmer/MainForm.cs
@@ -18,12 +18,33 @@ namespace Dimmer
             BrightnessController.MonitorArray[1].SetBrightness((int)255);
         }
 
-        private void Tbar_Scroll(object sender, System.EventArgs e)
+        private void TbarMonitorOne_Scroll(object sender, System.EventArgs e)
         {
             BrightnessController.MonitorArray[0].SetBrightness((int)tbarMonitorOne.Value);
-            BrightnessController.MonitorArray[1].SetBrightness((int)tbarMonitorOne.Value);
         }
-    }
+
+		private void TbarMonitorTwo_Scroll(object sender, System.EventArgs e)
+        {
+            BrightnessController.MonitorArray[1].SetBrightness((int)tbarMonitorTwo.Value);
+        }
+
+		private void BtnReset_Click(object sender, System.EventArgs e)
+		{
+            ResetDefaultBrightness();
+        }
+
+        private void ResetDefaultBrightness()
+		{
+            BrightnessController.MonitorArray[0].SetBrightness((int)255);
+            BrightnessController.MonitorArray[1].SetBrightness((int)255);
+
+            tbarMonitorOne.Value = tbarMonitorOne.Maximum;
+            tbarMonitorTwo.Value = tbarMonitorTwo.Maximum;
+        }
+
+		private void Dimmer_FormClosing(object sender, FormClosingEventArgs e)
+		{
+            ResetDefaultBrightness();
+		}
+	}
 }
-
-
